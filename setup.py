d328170e4280b16867dd333d9e664af403996be5
Austin Howard <ahoward@utdallas.edu>
2014-04-01 02:12:42 -0500

    Updated setup.py for pypi, also get version from kaplot/__init__.py directly

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+from kaplot import __version__
 
 setup(
 	name = 'kaplot',
-	version = '0.7',
+	version = __version__,
 	packages = find_packages(),
-	install_requires = ['scipy','numpy','matplotlib','decorator']
+	install_requires = ['scipy','numpy','matplotlib','decorator'],
+
+	author = 'Kamil Mielczarek',
+	author_email = 'kamil.m@gmail.com',
+	url = 'http://github.com/kamilm/kaplot',
+	description = 'A plotting tool built around/on matplotlib',
 )
