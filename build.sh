d4e4d7f2b1a94a63103c683ced155a2353627ae4
Kamil Mielczarek <kamil.m@gmail.com>
2014-04-01 14:50:06 -0500

    cloned from @austin1howard

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pdoc --html --overwrite --html-newline-br kaplot_backend
+pdoc --html --overwrite --html-newline-br kaplot
+pdoc --html --overwrite --html-newline-br kaplot.defaults       
+
